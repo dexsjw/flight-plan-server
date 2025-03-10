9372e04987a9a97c2731f624c87a4286a067a561
dextersjw <dextersjw@gmail.com>
2025-03-11 03:47:52 +0800

    added RestTemplate configuration

---
+++ src/main/java/challenge/tech/aviation/flight_plan_server/configs/RestTemplateConfig.java
@@ -0,0 +1,24 @@
+package challenge.tech.aviation.flight_plan_server.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Value("${external.flight.data.apikey}")
+    private String apikey;
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.connectTimeout(Duration.ofMillis(5000))
+                .readTimeout(Duration.ofMillis(5000))
+                .defaultHeader("apikey", apikey)
+                .build();
+    }
+}
