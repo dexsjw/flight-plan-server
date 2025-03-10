8c0febdb6903a12313dc247dd39780752d839e33
dextersjw <dextersjw@gmail.com>
2025-03-11 03:49:21 +0800

    created FlightPlanDto

---
+++ src/main/java/challenge/tech/aviation/flight_plan_server/dto/FlightPlanDto.java
@@ -0,0 +1,14 @@
+package challenge.tech.aviation.flight_plan_server.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FlightPlanDto {
+
+    private String _id;
+
+}
