de726d0615e254c304e390b73cf3f44615fe964a
Soh <dexsjw@gmail.com>
2025-03-10 16:05:11 +0800

    initial commit

---
+++ src/main/java/challenge/tech/aviation/flight_plan_server/FlightPlanServerApplication.java
@@ -0,0 +1,13 @@
+package challenge.tech.aviation.flight_plan_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPlanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPlanServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/tech/aviation/flight_plan_server/FlightPlanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.tech.aviation.flight_plan_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPlanServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
