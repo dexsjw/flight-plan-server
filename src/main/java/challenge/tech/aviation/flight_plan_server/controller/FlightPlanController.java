28f16e75caca231d1f2cd48ffdd8be551fb8f351
dextersjw <dextersjw@gmail.com>
2025-03-12 03:28:42 +0800

    added /displayAll endpoint to FlightPlanController

--- src/main/java/challenge/tech/aviation/flight_plan_server/controller/FlightPlanController.java
+++ src/main/java/challenge/tech/aviation/flight_plan_server/controller/FlightPlanController.java
@@ -1,10 +1,15 @@
 package challenge.tech.aviation.flight_plan_server.controller;
 
+import challenge.tech.aviation.flight_plan_server.dto.FlightPlanDto;
 import challenge.tech.aviation.flight_plan_server.service.FlightPlanService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/flight-plan")
@@ -19,6 +24,11 @@ public class FlightPlanController {
     @GetMapping("/test")
     public ResponseEntity<String> testEndpoint() {
         return flightPlanService.testEndpoint();
+    }
+
+    @GetMapping("/displayAll")
+    public ResponseEntity<List<FlightPlanDto>> displayAllFlightPlans() {
+        return ResponseEntity.status(HttpStatus.OK).body(flightPlanService.displayAllFlightPlans());
     }
 
 }
