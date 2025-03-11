847ae7e858a8bd472d1fda97d11f0643b9c6856d
Soh <dexsjw@gmail.com>
2025-03-11 18:24:09 +0800

    added WebClient to FlightPlanServiceImpl

--- src/main/java/challenge/tech/aviation/flight_plan_server/service/impl/FlightPlanServiceImpl.java
+++ src/main/java/challenge/tech/aviation/flight_plan_server/service/impl/FlightPlanServiceImpl.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.time.Duration;
 
 @Service
 public class FlightPlanServiceImpl implements FlightPlanService {
@@ -17,13 +20,22 @@ public class FlightPlanServiceImpl implements FlightPlanService {
 
     private final RestTemplate restTemplate;
 
-    public FlightPlanServiceImpl(RestTemplate restTemplate) {
+    private WebClient flightManagerWebClient;
+
+    public FlightPlanServiceImpl(RestTemplate restTemplate, WebClient flightManagerWebClient) {
+        this.flightManagerWebClient = flightManagerWebClient;
         this.restTemplate = restTemplate;
     }
 
     @Override
     public ResponseEntity<String> testEndpoint() {
         return restTemplate.getForEntity(flightManagerUrl + flightManagerPathDisplayAll, String.class);
+//        return flightManagerWebClient.get()
+//                .uri(flightManagerPathDisplayAll)
+//                .retrieve()
+//                .toEntity(String.class)
+//                .timeout(Duration.ofMillis(10000))
+//                .block();
     }
 
 }
