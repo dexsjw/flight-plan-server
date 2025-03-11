e90a7e1b841874af6822bf00ef55540829dd394a
dextersjw <dextersjw@gmail.com>
2025-03-12 03:30:01 +0800

    created service method for /displayAll

--- src/main/java/challenge/tech/aviation/flight_plan_server/service/FlightPlanService.java
+++ src/main/java/challenge/tech/aviation/flight_plan_server/service/FlightPlanService.java
@@ -1,7 +1,11 @@
 package challenge.tech.aviation.flight_plan_server.service;
 
+import challenge.tech.aviation.flight_plan_server.dto.FlightPlanDto;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface FlightPlanService {
     ResponseEntity<String> testEndpoint();
+    List<FlightPlanDto> displayAllFlightPlans();
 }
--- src/main/java/challenge/tech/aviation/flight_plan_server/service/impl/FlightPlanServiceImpl.java
+++ src/main/java/challenge/tech/aviation/flight_plan_server/service/impl/FlightPlanServiceImpl.java
@@ -1,30 +1,33 @@
 package challenge.tech.aviation.flight_plan_server.service.impl;
 
+import challenge.tech.aviation.flight_plan_server.dto.FlightPlanDto;
 import challenge.tech.aviation.flight_plan_server.service.FlightPlanService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
+import java.util.List;
 
 @Service
 public class FlightPlanServiceImpl implements FlightPlanService {
 
-    @Value("${external.flight.data.url.flight-manager}")
-    private String flightManagerUrl;
+//    @Value("${external.flight.data.url.flight-manager}")
+////    private String flightManagerUrl;
 
     @Value("${external.flight.data.path.flight-manager.display-all}")
     private String flightManagerPathDisplayAll;
 
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
 
     private WebClient flightManagerWebClient;
 
     public FlightPlanServiceImpl(RestTemplate restTemplate, WebClient flightManagerWebClient) {
         this.flightManagerWebClient = flightManagerWebClient;
-        this.restTemplate = restTemplate;
+//        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -34,6 +37,16 @@ public class FlightPlanServiceImpl implements FlightPlanService {
                 .uri(flightManagerPathDisplayAll)
                 .retrieve()
                 .toEntity(String.class)
+                .timeout(Duration.ofMillis(10000))
+                .block();
+    }
+
+    @Override
+    public List<FlightPlanDto> displayAllFlightPlans() {
+        return flightManagerWebClient.get()
+                .uri(flightManagerPathDisplayAll)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<FlightPlanDto>>() {})
                 .timeout(Duration.ofMillis(10000))
                 .block();
     }
