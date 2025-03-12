eea07105a28068f9e8f9a15b4e44ee8514ecea3c
dextersjw <dextersjw@gmail.com>
2025-03-13 05:11:19 +0800

    updated PositionDto with pointCoordinate for coordinates

--- src/main/java/challenge/tech/aviation/flight_plan_server/dto/PositionDto.java
+++ src/main/java/challenge/tech/aviation/flight_plan_server/dto/PositionDto.java
@@ -3,6 +3,8 @@ package challenge.tech.aviation.flight_plan_server.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -11,5 +13,6 @@ import lombok.*;
 public class PositionDto {
 
     private String designatedPoint;
+    private String pointCoordinate;
 
 }
