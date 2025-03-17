24b692dd4b67dd82eb2116c0ba41aef105b6d5e1
dextersjw <dextersjw@gmail.com>
2025-03-17 10:29:09 +0800

    enabled CORS

--- src/main/java/challenge/tech/aviation/flight_plan_server/controller/FlightPlanController.java
+++ src/main/java/challenge/tech/aviation/flight_plan_server/controller/FlightPlanController.java
@@ -5,14 +5,12 @@ import challenge.tech.aviation.flight_plan_server.dto.FlightPlanRouteDataDto;
 import challenge.tech.aviation.flight_plan_server.service.FlightPlanService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173/")
 @RestController
 @RequestMapping("/flight-plan")
 public class FlightPlanController {
